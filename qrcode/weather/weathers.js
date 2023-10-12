f333f6fb77b44dd397e0fd69d585eab269884e8c
Bikash Ghimire <bikashghimire@Bikashs-MacBook-Air.local>
2023-10-12 13:05:58 +0545

    time

--- qrcode/weather/weathers.js
+++ qrcode/weather/weathers.js
@@ -3,19 +3,18 @@
 
 const axios = require("axios")
 
+// api 
 const url = "https://api.open-meteo.com/v1/forecast?latitude=12.9184&longitude=79.1325&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&timezone=GMT";
 
 axios.get(url)
   .then((response) => {
-    const { daily } = response.data
-    // console.log({daily})
+    // destructing the  daily ,hourly_units ,hourly from response.data 
+    const { daily, hourly_units, hourly } = response.data
+    console.log({ hourly_units, hourly, daily, })
 
-    const {sunrise,sunset} = daily;
-    console.log({sunrise : sunrise.toString() , sunset:sunset.toString()});
-    // console.log({ hourly_units, hourly, daily, ...rest })
-
-
-
+    // destructing the sunrise and sunset from the daily 
+    const { sunrise, sunset } = daily;
+    console.log({ sunrise: sunrise.toString(), sunset: sunset.toString() });
   })
   .catch(function (error) {
     console.log({ error });
