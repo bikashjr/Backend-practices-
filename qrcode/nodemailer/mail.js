e237f4629017c98c5bdd1a60524283446808d1c7
Bikash Ghimire <bikashghimire@Bikashs-MacBook-Air.local>
2023-10-14 12:25:34 +0545

    ternary operator

--- qrcode/nodemailer/mail.js
+++ qrcode/nodemailer/mail.js
@@ -15,7 +15,24 @@ const transporter = nodemailer.createTransport({
       to: "ghimirebikash41@gmail.com",
       subject: "Hello ✔", 
       text: "Hello world?", 
-      html: "<b>Hello world?</b>"
+      html: "<b>Hello world?</b>",
+      attachments: [
+        {
+            filename: 'note.txt',
+            // my content here
+            content: 'Some notes about this e-mail',  
+            contentType:'text/plain'
+        },
+        {
+            filename: 'image.png',
+            content: Buffer.from(
+                'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUAAAD/' +
+                    '//+l2Z/dAAAAM0lEQVR4nGP4/5/h/1+G/58ZDrAz3D/McH8yw83NDDeNGe4U' +
+                    'g9C9zwz3gVLMDA/A6P9/AFGGFyjOXZtQAAAAAElFTkSuQmCC',
+                'base64'
+            ),
+        },
+    ],
 });
   
     console.log("Message sent: %s", info.messageId);
---
+++ qrcode/nodemailer/operator.js
@@ -0,0 +1,32 @@
+// let x = 10;
+// let z;
+
+// (x===10) ? z="true": z="false";
+
+//  z= (x===10) ? "true":"false";
+
+// z = " Value is " + (x === 10 ? "true" : "false");
+
+
+// const x = "ram" ;
+// let result;
+//  result = x==="ram" ? "ram is hancy" :"ram is ugly"
+
+const number = 0;
+let result;
+
+// if(number === 0){
+//     result = "0 point "
+// }else if (number <10){
+//     result = "10 point"
+// }
+// else{
+//    result =  "You have alots"
+// }
+
+result = number === 0 ? "0point" : (
+    number < 10 ? "10 points" : "100 points"
+)
+console.log({ result });
+
+
